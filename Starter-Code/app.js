6adf8e0c20b7783c285c7f8f5929041094dcabdc
KnightRider <omar.brown@comcast.net>
2019-04-08 12:12:24 -0400

    last last changes

--- Starter-Code/app.js
+++ Starter-Code/app.js
@@ -1,1 +1,15 @@
 // Your jQuery Logic will go here
+// on button click add input value to the ordered lists
+
+$('button').click(()=>{
+    event.preventDefault()
+    let input = $('input:text').val()
+    $('ol').append('<li class="strik">'+ input + "</li>")
+    $('input').val('')
+}
+)
+$('ol').on("click", ".strik", ()=>{
+    $(event.target).css('text-decoration','line-through');
+});
+// on button click add check , or button down to strike through
+
